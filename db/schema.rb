49cbeae042d0f978b584332b50155dd086296cdb
Simran pal <simranp@thoughtworks.com>
2016-05-27 10:50:16 +0530

    added a migration for adding price column to books

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160512143148) do
+ActiveRecord::Schema.define(version: 20160526122839) do
 
   create_table "books", force: :cascade do |t|
     t.string   "title"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20160512143148) do
     t.string   "author"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
+    t.integer  "price"
   end
 
   create_table "widgets", force: :cascade do |t|
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,5 +6,7 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-books = Book.create(title: 'The Little Prince',no_of_copies: 4,author: "Andrew Clements")
-Book.create(title: 'Frindle',no_of_copies: 4,author: "Andrew Clements")
+books = Book.create(title: 'The Little Prince', no_of_copies: 4, author: 'Andrew Clements', price:300)
+Book.create(title: 'Frindle', no_of_copies: 4, author: 'Andrew Clements', price:300)
+Book.create(title: 'The Zen Pencils', no_of_copies: 4, author: 'Gavin Aung Than', price:800)
+Book.create(title: 'Yes, My Accent is Real', no_of_copies: 0, author: 'Kunal Nayyar', price:300)
