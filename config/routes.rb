94485d98256f47bd5999d94083658cd298b44706
Simran pal <simranp@thoughtworks.com>
2016-06-10 19:26:34 +0530

    removed .idea

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  resources :books, :only => [:index] do
-
-  end
+  get 'books' => 'books#index'
+  # resources :books, :only => [:index] do
+  #
+  # end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
