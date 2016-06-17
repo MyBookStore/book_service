57a385333d4e73b6147f9e80f43c2b2a8c984228
Simran pal <simranp@thoughtworks.com>
2016-06-17 09:14:55 +0530

    added end point for getting one book of given id

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,4 +2,8 @@ class BooksController < ApplicationController
   def index
     render json: Book.all
   end
+
+  def get
+    render json: Book.find_by_id(params[:book][:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get 'books' => 'books#index'
+  get 'books/:id' => 'books#get'
   # resources :books, :only => [:index] do
   #
   # end
